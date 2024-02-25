6d6c88d9195c970a3e898a61d270e07f29ef78cc
Delfio Francisco <delfio_eu@hotmail.com>
2024-02-24 22:39:56 -0400

    fix stress test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import httpServer from "./src/http-interface/http-server.js";
 import tcpServer from "./src/http-interface/tcp-server.js";
 import cluster from "node:cluster";
 import stressTest from './pre-stress-test.js'
-import {setTimeout} from 'node:timers/promises'
+import { setTimeout } from 'node:timers/promises'
 
 const numCPUs = 2
 
@@ -12,6 +12,7 @@ const TCP_HOST = process.env.TCP_HOST || 'localhost'
 const TCP_PORT = process.env.TCP_PORT || 3301
 const HTTP_PORT = process.env.HTTP_PORT || 3000
 
+const INSTANCE = process.env.INSTANCE
 if (cluster.isPrimary) {
     console.log(`Master ${process.pid} is running`);
 
@@ -20,13 +21,17 @@ if (cluster.isPrimary) {
     }
 
     await setTimeout(2000) // por garantia
-    await stressTest('localhost', HTTP_PORT)
+
+    if (INSTANCE == "HTTP_SERVER") {
+        await stressTest('localhost', HTTP_PORT)
+    }
+
 } else {
     console.log(`worker ${process.pid} is running`)
 
-    if (process.env.INSTANCE == "TCP_SERVER") {
+    if (INSTANCE == "TCP_SERVER") {
         tcpServer(TCP_PORT)
-    } else if (process.env.INSTANCE == "HTTP_SERVER") {
+    } else if (INSTANCE == "HTTP_SERVER") {
         httpServer(HTTP_PORT, TCP_PORT, TCP_HOST)
     } else {
         tcpServer(TCP_PORT)
